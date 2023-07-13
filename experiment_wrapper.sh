eae5385d1a6978a4d6a7768b5ae4a5226752dfc0
justinyeh1995 <justinyeh1995@gmail.com>
2023-07-12 19:21:10 -0500

    run_debug working with HelloWorldJava

--- experiment_wrapper.sh
+++ experiment_wrapper.sh
@@ -1,6 +1,8 @@
+systemctl start mongod
+
 cd /home/cpswt/webgme-engine
 
-node src/bin/import.js -m mongodb://127.0.0.1:27017/c2webgme -p HelloWorldJava ../cpswt-meta/seeds/_archive/CPSWT_Helloworld_Java_Tutorial
+node src/bin/import.js -m mongodb://127.0.0.1:27017/c2webgme -p HelloWorldOmnetpp ../cpswt-meta/seeds/_archive/CPSWT_Helloworld_Java_Tutorial
 
 cd /home/cpswt/cpswt-meta
 
@@ -9,6 +11,12 @@ npm start &
 
 sleep 10
 
-export PYTHONPATH=$PWD/src/plugins/PyFederatesExporter
-python3 $PWD/src/plugins/PyFederatesExporter/PyFederatesExporter/run_debug.py
+# generate .zip file
+nvm use 8.10.0
+export PYTHONPATH=$PWD/src/plugins/PyFederatesExporter/PyFederatesExporter
+python3 $PWD/src/plugins/PyFederatesExporter/run_debug.py
+
+#unzip .zip
+cd /tmp
+
 
