bf6a06eb04c2cb4c0ac1b12acc70e8cae03aceda
justinyeh1995 <justinyeh1995@gmail.com>
2023-07-18 14:29:01 -0500

    containerize cpswt-cpp (unproven)

---
+++ cpswt-cpp/experiment_wrapper.sh
@@ -0,0 +1,65 @@
+# this script is used to build cpswt-core and its dependencies in the docker container 
+ORIGINAL_PATH=$PATH
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-arm64
+export PATH=$JAVA_HOME/bin:$PATH
+/opt/apache-archiva-2.2.5/bin/archiva start
+
+# wait for archiva to start
+# nc -zv localhost 8080
+sleep 20
+# create admin user
+curl --no-progress-meter -X POST -H "Content-Type: application/json" -H "Origin: http://localhost:8080" -d @- \
+ http://localhost:8080/restServices/redbackServices/userService/createAdminUser <<'TERMINUS'
+{
+    "username": "admin",
+    "password": "adminpass123",
+    "email": "admin@archiva-test.org",
+    "fullName": "Admin",
+    "locked": false,
+    "passwordChangeRequired": false,
+    "permanent": false,
+    "readOnly": false,
+    "validated": true,
+    "confirmPassword": "adminpass123"
+}
+TERMINUS
+
+# switch to java 17
+unset JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64
+export PATH=$JAVA_HOME/bin:$ORIGINAL_PATH
+
+# clone cpswt-core and build it
+cd /home/cpswt
+git clone https://github.com/justinyeh1995/cpswt-core.git
+cd cpswt-core/cpswt-core
+
+gradle wrapper --gradle-version=7.5
+
+./gradlew :utils:publish 
+./gradlew :root:publish
+./gradlew :base-events:publish 
+./gradlew :config:publish
+./gradlew :federate-base:publish 
+./gradlew :coa:publish 
+./gradlew :federation-manager:publish 
+./gradlew :fedmanager-host:publish 
+
+# build cpswt-cpp
+cd /home/cpswt
+git clone git@github.com:SimIntToolkit/cpswt-cpp.git
+cd cpswt-cpp
+
+gradle wrapper --gradle-version=7.5
+
+./gradlew :foundation:CppTestHarness:publish
+./gradlew :foundation:core-cpp:publish
+./gradlew :foundation:C2WConsoleLogger:publish
+./gradlew :foundation:rti-base-cpp:publish
+./gradlew :foundation:CPSWTConfig:publish
+./gradlew :foundation:SynchronizedFederate:publish
+
+cd /home/cpswt/cpswt-cpp/examples/HelloWorldCpp
+gradle wrapper --gradle-version=7.5
+
+./gradlew :runFederationBatch
