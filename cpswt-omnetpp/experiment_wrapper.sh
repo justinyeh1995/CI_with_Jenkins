b8d748dd934d38ac1bf599ec00425c47aef86b93
justinyeh1995 <justinyeh1995@gmail.com>
2023-07-20 13:22:31 -0500

    gradle -b build.dummy.kts

--- cpswt-omnetpp/experiment_wrapper.sh
+++ cpswt-omnetpp/experiment_wrapper.sh
@@ -1,12 +1,18 @@
 # this script is used to build cpswt-core and its dependencies in the docker container 
 ORIGINAL_PATH=$PATH
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-arm64
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 /opt/apache-archiva-2.2.5/bin/archiva start
 
 # wait for archiva to start
-# nc -zv localhost 8080
-sleep 20
+echo "Waiting archiva to launch on 8080..."
+
+while ! nc -z localhost 8080; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo "archiva launched"
+
 # create admin user
 curl --no-progress-meter -X POST -H "Content-Type: application/json" -H "Origin: http://localhost:8080" -d @- \
  http://localhost:8080/restServices/redbackServices/userService/createAdminUser <<'TERMINUS'
@@ -26,7 +32,7 @@ TERMINUS
 
 # switch to java 17
 unset JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64
+export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$ORIGINAL_PATH
 
 # clone cpswt-core and build it
@@ -50,7 +56,7 @@ cd /home/cpswt
 git clone git@github.com:SimIntToolkit/cpswt-cpp.git
 cd cpswt-cpp
 
-gradle wrapper --gradle-version=7.5
+gradle wrapper --gradle-version=8.0
 
 ./gradlew :foundation:CppTestHarness:publish
 ./gradlew :foundation:core-cpp:publish
@@ -96,6 +102,9 @@ cd cpswt-omnetpp
 # Build the OmnetFederate:
 ./gradlew :foundation:OmnetFederate:build
 export OMNETPP_CPSWT_HOME=/home/cpswt/cpswt-omnetpp/foundation/OmnetFederate
-
-gradle wrapper --gradle-version=7.5
+touch build.dummy.kts
+gradle -b build.dummy.kts wrapper --gradle-version=8.0
+./gradlew :Source:build
+./gradlew :Sink:build
+./gradlew :PingCounter:build
 ./gradlew :runFederationBatch
