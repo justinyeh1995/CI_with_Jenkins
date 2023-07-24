4afe2fc495ed0761999ccd9595876fdad8421dd8
justinyeh1995 <justinyeh1995@gmail.com>
2023-07-23 23:56:40 -0500

    add HelloWorldJava

---
+++ cpswt-meta/HelloWorldJava_wrapper.sh
@@ -0,0 +1,60 @@
+# this script is used to build cpswt-core and its dependencies in the docker container 
+ORIGINAL_PATH=$PATH
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+/opt/apache-archiva-2.2.5/bin/archiva start
+
+# wait for archiva to start
+# wait for archiva to start
+echo "Waiting archiva to launch on 8080..."
+
+while ! nc -z localhost 8080; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo "archiva launched"
+
+# create admin user
+curl --no-progress-meter -X POST -H "Content-Type: application/json" -H "Origin: http://localhost:8080" -d @- \
+ http://localhost:8080/restServices/redbackServices/userService/createAdminUser <<'TERMINUS'
+{
+    "username": "admin",
+    "password": "adminpass123",
+    "email": "admin@archiva-test.org",
+    "fullName": "Admin",
+    "locked": false,
+    "passwordChangeRequired": false,
+    "permanent": false,
+    "readOnly": false,
+    "validated": true,
+    "confirmPassword": "adminpass123"
+}
+TERMINUS
+
+# switch to java 17
+unset JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$ORIGINAL_PATH
+
+# clone cpswt-core and build it
+cd /home/cpswt
+git clone https://github.com/SimIntToolkit/cpswt-core.git
+cd cpswt-core/cpswt-core
+
+gradle wrapper --gradle-version=7.5
+
+./gradlew :utils:publish 
+./gradlew :root:publish
+./gradlew :base-events:publish 
+./gradlew :config:publish
+./gradlew :federate-base:publish 
+./gradlew :coa:publish 
+./gradlew :federation-manager:publish 
+./gradlew :fedmanager-host:publish
+
+cd /home/cpswt/HelloWorldJava
+gradle wrapper --gradle-version=7.5
+./gradlew :Source:build
+./gradlew :Sink:build
+./gradlew :PingCounter:build
+./gradlew :runFederationBatch
